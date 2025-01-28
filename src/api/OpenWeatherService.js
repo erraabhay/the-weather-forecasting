e8136417898852536abbbc6cc7b592a0df748b1e
erraabhay <abhayerra25@outlook.com>
2025-01-28 22:11:48 +0530

    Update OpenWeatherService.js

--- src/api/OpenWeatherService.js
+++ src/api/OpenWeatherService.js
@@ -1,7 +1,7 @@
 const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
 
 const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5';
-const WEATHER_API_KEY = 'Your API KEY';
+const WEATHER_API_KEY = 'b8e95a5020d5b2ad66006021e341b05a';
 
 const GEO_API_OPTIONS = {
   method: 'GET',
